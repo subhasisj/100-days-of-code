22989d2369288217905197ab5e79f6a3bb3ac430
Subhasis Jethy <46371699+subhasisj@users.noreply.github.com>
2022-01-13 00:31:29 +0530

    Time: 198 ms (9.64%), Space: 15.4 MB (95.45%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.py
@@ -9,13 +9,11 @@ class Solution:
         left = 1
         right = 1
         for i in range(1,len(nums)):
-            # left = right = nums[i]
             if nums[i-1] != nums[i]:
                 nums[left] = nums[i]
                 left+=1
                 
 #         delete elements backwards
-        print(i)
         for delete_index in range(i,left-1,-1):
             del nums[delete_index]
         
