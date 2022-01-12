362499cdf7994b960173c26ac3b06514f87fd86d
Subhasis Jethy <46371699+subhasisj@users.noreply.github.com>
2022-01-12 23:44:43 +0530

    Time: 48 ms (72.45%), Space: 14.4 MB (28.34%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,13 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         symbol_value =  {'I':1, 'V': 5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        value = 0
+        sum = 0
         for i in range(1,len(s)):
             if symbol_value[s[i-1]] < symbol_value[s[i]]:
-                value -= symbol_value[s[i-1]]
+                sum -= symbol_value[s[i-1]]
             else:
-                value += symbol_value[s[i-1]]
-        value += symbol_value[s[-1]]
-        return value
-                
+                sum += symbol_value[s[i-1]]
+        sum+= symbol_value[s[-1]]      
+        return sum
+        
         
