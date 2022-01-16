f4be37135ddd6d0417b7e18fb0ec4df50646e666
Subhasis Jethy <46371699+subhasisj@users.noreply.github.com>
2022-01-16 22:50:13 +0530

    Time: 772 ms (86.25%), Space: 17.6 MB (49.01%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+        
+        for i,a in enumerate(nums):
+            # if its not the 1st element and it is the same as the next one
+            if i>0 and a == nums[i-1]: 
+                continue
+             
+            l,r = i+1, len(nums)-1 
+            while l<r:
+                threesum = a + nums[l] + nums[r]
+                if threesum > 0: # if the sum < 0 then the expected sum has to be with an element in the left as this is sorted
+                    r-=1
+                elif threesum < 0: # if the sum < 0 then the expected sum has to be with an element in the right as this is sorted
+                    l+=1
+                else:
+                    res.append([a,nums[l],nums[r]])
+                    l+=1
+                    while nums[l] == nums[l-1] and l<r:
+                        l+=1
+        return res
+                    
+                                 
+                                 
